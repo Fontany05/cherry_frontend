4fb932014e9e18f7bedb7de3075edd921948c97a
anaf <any_822@hotmail.com>
2025-06-13 19:47:53 -0300

    creacion simple de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.component'),
+        children: [
+            {
+                path: 'products-list',
+                title: 'Products',
+                loadComponent:() => import('./dashboard/pages/products/products.component'),
+            },
+            {
+                path: 'contact',
+                title: 'Contact',
+                loadComponent: () => import('./dashboard/pages/contact/contact.component'),
+            },
+            {
+                path: 'product/:id',
+                title: 'Product-item',
+                loadComponent:() => import('./dashboard/pages/product/product.component'),
+            },
+
+        ] 
+    }, {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    }
+
+
+];
