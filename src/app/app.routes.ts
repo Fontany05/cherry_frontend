4387998eee149217c14785c009975f5a95ef4059
anaf <any_822@hotmail.com>
2025-06-26 17:04:30 -0300

    modifcaciones de los directorios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,13 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 export const routes: Routes = [
-
-    {
-        path: 'dashboard',
-        loadComponent: () => import('./dashboard/dashboard.component'),
-        children: [
-            {
-                path: 'products-list',
-                title: 'Products',
-                loadComponent:() => import('./dashboard/pages/products/products.component'),
-            },
-            {
-                path: 'contact',
-                title: 'Contact',
-                loadComponent: () => import('./dashboard/pages/contact/contact.component'),
-            },
-            {
-                path: 'product/:id',
-                title: 'Product-item',
-                loadComponent:() => import('./dashboard/pages/product/product.component'),
-            },
-
-        ] 
-    }, {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-    }
-
-
+  {
+    path: '',
+    component: HeaderComponent
+   },
+   {
+    path: '**',
+    redirectTo: ''
+   }
 ];
