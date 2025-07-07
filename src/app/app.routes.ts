55a73e27febc17cad8fadbdee70c3921ddd6a93f
anaf <any_822@hotmail.com>
2025-07-07 14:33:26 -0300

    modificacion de rutas,sume login y register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,27 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './components/shared/header/header.component';
+import ContactComponent from './pages/contact/contact.component';
+import ProductsComponent from './pages/product/product.component';
+import LoginComponent from './pages/login/login.component'
+import RegisterComponent  from './pages/register/register.component';
+
 
 export const routes: Routes = [
   {
-    path: '',
-    component: HeaderComponent
+    path: 'products',
+    component: ProductsComponent
    },
-   {
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  { path: 'login', 
+    component: LoginComponent },
+
+  { path: 'register', 
+    component: RegisterComponent },
+
+  {
     path: '**',
     redirectTo: ''
-   }
+  }
 ];
