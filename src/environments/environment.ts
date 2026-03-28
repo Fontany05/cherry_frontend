2136aea9b60ddb41ce30f302f53452e677ad6caa
anaf <any_822@hotmail.com>
2026-03-27 23:41:36 -0300

    agregar environment.ts for production

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://cherry-backend-ekr9.onrender.com/api',
+  stripePublicKey:
+    'pk_test_51SOQNA3LzoorKagKG0aUMvIysKc5PDZNGgJsqbYkBQ6KfKQTER08HGO3c3c3qvUXNNQZpUXCREB6dTtsnsHKfcOP00FR9ST4sQ',
+};
